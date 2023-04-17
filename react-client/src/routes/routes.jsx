faea5c061deaf78d2951c67d6a7e7af80c770c64
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-04-17 19:57:45 +0200

    adding routes

--- react-client/src/routes/routes.jsx
+++ react-client/src/routes/routes.jsx
@@ -1,11 +1,16 @@
 import Dashboard from '../components/admin/Dashboard';
 import Products from '../components/admin/Products';
-// import { Navigate } from 'react-router-dom';
+import Category from '../components/admin/category/Category';
+import ViewCategory from '../components/admin/category/ViewCategory';
+import EditCategory from '../components/admin/category/EditCategory';
 
 const routes = [
     { path: '/admin', exact: true, name:'Admin', },
     { path: 'dashboard', exact: true, name:'Dashboard', component: Dashboard },
-    { path: 'products', exact: true, name:'Products', component: Products }
+    { path: 'products', exact: true, name:'Products', component: Products },
+    { path: 'add-category', exact: true, name:'Category', component: Category },
+    { path: 'edit-category/:id', exact: true, name:'EditCategory', component: EditCategory },
+    { path: 'view-category', exact: true, name:'ViewCategory', component: ViewCategory },
 ];
 
 export default routes;
