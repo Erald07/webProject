36d7a5a91e6878fb5798671baf6123ec434170aa
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-05-29 13:48:20 +0200

    added new route

--- react-client/src/routes/routes.jsx
+++ react-client/src/routes/routes.jsx
@@ -7,9 +7,11 @@ import Product from '../components/admin/product/Product';
 import User from '../components/admin/user/User';
 import ViewUser from '../components/admin/user/ViewUser';
 import EditProduct from '../components/admin/product/EditProduct';
+import Order from '../components/admin/order/Order';
+import ViewOrder from '../components/admin/order/ViewOrder';
 
 const routes = [
-    { path: '/admin', exact: true, name:'Admin', },
+    { path: '/admin', exact: true, name:'Admin' },
     { path: 'dashboard', exact: true, name:'Dashboard', component: Dashboard },
     { path: 'view-product', exact: true, name:'ViewProducts', component: ViewProduct },
     { path: 'edit-product/:id', exact: true, name:'EditProduct', component: EditProduct },
@@ -19,6 +21,8 @@ const routes = [
     { path: 'view-category', exact: true, name:'ViewCategory', component: ViewCategory },
     { path: 'view-user', exact: true, name:'ViewUser', component: ViewUser },
     { path: 'add-user', exact: true, name:'User', component: User },
+    { path: 'orders', exact: true, name:'Order', component: Order },
+    { path: 'view-order/:id', exact: true, name:'ViewOrder', component: ViewOrder },
 ];
 
 export default routes;
