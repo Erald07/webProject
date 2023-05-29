3cf81e347f9f3287df3859735137c0e135273689
Erald07 <98850032+Erald07@users.noreply.github.com>
2023-05-29 13:52:33 +0200

    abstract class for filter

---
+++ api-laravel/app/Filters/MaxPriceFilter.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Filters;
+
+class MaxPriceFilter
+{
+    public function filter($builder, $value)
+    {
+        return $builder->where('original_price', '<=', $value);
+    }
+}
---
+++ api-laravel/app/Filters/ProductFilter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filters;
+
+use App\Filters\AbstractFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class ProductFilter extends AbstractFilter
+{
+    protected $filters = [
+        'min_price' => MinPriceFilter::class,
+        'max_price' => MaxPriceFilter::class,
+    ];
+}
